1861a96e41930c7f9be6542bc0f2d78d0004f762
Mohamed Hossain <mz.hossain212@gmail.com>
2021-01-19 19:27:06 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -1,14 +1,16 @@
 def second_challenge
   epic_tragedy = {
-    :montague => {
-      :patriach => {
-        :name => "Lord Montague"
-        :age => "53"
-      }
-      },
+    :montague=> {
+      :patriarch => {},
+      :matriarch => {},
+      :hero: => {},
+      :hero_friends => []
+    },
     :capulet => {
-      #your key/value pairs here!
-      }}
-
-
+      :patriarch => {},
+      :matriarch => {},
+      :heroine: => {},
+      :heroine_friends => []
+      }
+    }
 end
